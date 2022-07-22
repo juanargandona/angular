e46723182e12768817c1c18de5b996b07cf0a2b7
jarga <juanargandona@gmail.com>
2022-07-22 07:53:49 -0500

    New project 04-spotiapp

---
+++ de-cero-a-experto/04-spotiapp/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+import { HelpComponent } from './components/help/help.component';
+
+// Importar rutas
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    ArtistaComponent,
+    NavbarComponent,
+    HelpComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ROUTES, { useHash: true})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
